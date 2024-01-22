3f43767e597c11b4668b6ed6eca209d62be74152
Ephraim Jan Conales <conalesej@gmail.com>
2024-01-22 15:31:21 +0800

    commiting messenger

--- app/components/ContactUs/ContactUs.tsx
+++ app/components/ContactUs/ContactUs.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import styles from "./ContactUs.module.scss";
 import {
@@ -9,6 +10,23 @@ import {
   MessengerIcon,
 } from "@/app/common";
 const ContactUs = () => {
+  const [windowWidth, setWindowWidth] = useState(() => {
+    // Use a callback to provide an initial value and check for SSR
+    return typeof window !== "undefined" ? window.innerWidth : 0;
+  });
+
+  const handleResize = () => {
+    setWindowWidth(typeof window !== "undefined" ? window.innerWidth : 0);
+  };
+  useEffect(() => {
+    // Add event listener to update window width on resize
+    window.addEventListener("resize", handleResize);
+
+    // Clean up the event listener when the component is unmounted
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <main className={styles.contactUsMain}>
       <Image
@@ -33,7 +51,13 @@ const ContactUs = () => {
           <div className={styles.contact}>
             <div className={styles.name}>Ephraim Conales</div>
             <div className={styles.icons}>
-              <MessengerIcon href="https://www.facebook.com/messages/t/conalesej" />
+              <MessengerIcon
+                href={`${
+                  window.innerWidth > 768
+                    ? "https://www.facebook.com/messages/t/conalesej"
+                    : "messenger://conalesej"
+                }`}
+              />
               <MailIcon href="sms:+63949699082" />
               <CellphoneIcon contactUs href="tel:+639496990828" />
             </div>
@@ -42,7 +66,13 @@ const ContactUs = () => {
           <div className={styles.contact}>
             <div className={styles.name}> Elaine Luceñada </div>
             <div className={styles.icons}>
-              <MessengerIcon href="https://www.facebook.com/messages/t/aine095" />
+              <MessengerIcon
+                href={`${
+                  window.innerWidth > 768
+                    ? "https://www.facebook.com/messages/t/aine095"
+                    : "messenger://aine095"
+                }`}
+              />
               <MailIcon href="sms:+639301997543" />
               <CellphoneIcon contactUs href="tel:+639301997543" />
             </div>
